a9e9e92a9ec4734e21ed3600129e4bce2ef0b0c3
Rainmire <mqyin@g.ucla.edu>
2018-02-20 17:34:53 -0800

    add app and root

---
+++ components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import UserSearchContainer from './user_search/user_search_container';
+
+const App = () => (
+  <div className="app">
+    <UserSearchContainer />
+  </div>
+);
+
+export default App;
---
+++ components/user_search/user_search_container.js
@@ -0,0 +1,3 @@
+import { connect } from 'react-redux';
+import UserSearch from './user_search';
+
