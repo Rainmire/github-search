20b9b6ab357f33d5f7056934e12d9ba3f4a38d5f
Rainmire <mqyin@g.ucla.edu>
2018-02-21 05:01:01 -0800

    add arrow key support

--- src/components/user_search/user_search_index.jsx
+++ src/components/user_search/user_search_index.jsx
@@ -5,18 +5,42 @@ class UserSearchIndex extends React.Component {
   constructor(props) {
     super(props);
     this.state = { selectedUser: 0 };
+    this.changeSelection = this.changeSelection.bind(this);
+    window.addEventListener('keydown', this.changeSelection, false);
+  }
+
+  changeSelection(e) {
+    const keyName = event.key;
+
+    if (keyName === 'ArrowUp') {
+      this.setState((prevState) => {
+        let newSelectedUser = prevState.selectedUser;
+        if (newSelectedUser > 0) {
+          newSelectedUser--;
+        }
+        return {selectedUser: newSelectedUser};
+      });
+    } else if (keyName === 'ArrowDown') {
+      this.setState((prevState) => {
+        let newSelectedUser = prevState.selectedUser;
+        if (newSelectedUser < 4) {
+          newSelectedUser++;
+        }
+        return {selectedUser: newSelectedUser};
+      });
+    }
   }
 
   userSearchItem(user, idx) {
-    let itemClass = "user-search-item";
+    let itemClass = 'user-search-item';
     if (idx === this.state.selectedUser) {
-      itemClass += " selected-item";
+      itemClass += ' selected-item';
     }
     return (
       <li className={itemClass} key={idx}>
-        <a href={user.html_url} className="user-search-link">
-          <img className="user-search-avatar" src={user.avatar_url} alt="avatar"/>
-          <div className="user-search-username">{user.login}</div>
+        <a href={user.html_url} className='user-search-link'>
+          <img className='user-search-avatar' src={user.avatar_url} alt='avatar'/>
+          <div className='user-search-username'>{user.login}</div>
         </a>
       </li>
     )
@@ -38,11 +62,15 @@ class UserSearchIndex extends React.Component {
         <li>No results found</li>;
     }
 
-    return(
-      <ul className="user-search-list">
+    return (
+      <ul className='user-search-list'>
         {listItems}
       </ul>
     );
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.changeSelection, false);
   }
 
 }
