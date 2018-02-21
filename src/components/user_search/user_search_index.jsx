efff261f6d4e1285b828cf3f85b2518a917a3cba
Rainmire <mqyin@g.ucla.edu>
2018-02-21 04:28:58 -0800

    add item selection

--- src/components/user_search/user_search_index.jsx
+++ src/components/user_search/user_search_index.jsx
@@ -1,30 +1,50 @@
 import React from 'react';
 
-export default ({searchResults}) => {
+class UserSearchIndex extends React.Component {
 
-  let listItems;
-
-  if (searchResults.length !== 0) {
-    listItems =
-      searchResults.map(
-        (user) => (
-          <li key={user.id}>
-            <a href={user.html_url} className="user-search-link">
-              <img className="user-search-avatar" src={user.avatar_url} alt="avatar"/>
-              <div className="user-search-username">{user.login}</div>
-            </a>
-          </li>
-        )
-      );
-  } else {
-    listItems =
-      <li>No results found</li>;
+  constructor(props) {
+    super(props);
+    this.state = { selectedUser: 0 };
   }
 
+  userSearchItem(user, idx) {
+    let itemClass = "user-search-item";
+    if (idx === this.state.selectedUser) {
+      itemClass += " selected-item";
+    }
+    return (
+      <li className={itemClass} key={idx}>
+        <a href={user.html_url} className="user-search-link">
+          <img className="user-search-avatar" src={user.avatar_url} alt="avatar"/>
+          <div className="user-search-username">{user.login}</div>
+        </a>
+      </li>
+    )
+  }
 
-  return(
-    <ul className="user-search-list">
-      {listItems}
-    </ul>
-  );
-};
+  render() {
+    const { searchResults } = this.props;
+    let listItems;
+
+    if (searchResults.length !== 0) {
+      listItems =
+        searchResults.map(
+          (user, idx) => (
+            this.userSearchItem(user, idx)
+          )
+        );
+    } else {
+      listItems =
+        <li>No results found</li>;
+    }
+
+    return(
+      <ul className="user-search-list">
+        {listItems}
+      </ul>
+    );
+  }
+
+}
+
+export default UserSearchIndex;
