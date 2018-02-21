6cf7d439c0c808042037d8377ecfededdc8e3f43
Rainmire <mqyin@g.ucla.edu>
2018-02-20 17:39:04 -0800

    add user_search_container

--- components/user_search/user_search_container.js
+++ components/user_search/user_search_container.js
@@ -1,3 +1,15 @@
 import { connect } from 'react-redux';
 import UserSearch from './user_search';
 
+const mapStateToProps = (state) => ({
+  //STATE
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  //API call action
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UserSearch);
