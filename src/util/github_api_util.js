91e3892d224a76e309e189589de8bf191200499b
Rainmire <mqyin@g.ucla.edu>
2018-02-21 10:54:01 -0800

    update token

--- src/util/github_api_util.js
+++ src/util/github_api_util.js
@@ -3,6 +3,6 @@ const $ = require ('jquery');
 export const fetchUsers = (query) => (
   $.ajax({
     method: 'GET',
-    url: `https://api.github.com/search/users?q=${query}&access_token=ca1901d12ef6572be359156bf10f5ee560112d4b`
+    url: `https://api.github.com/search/users?q=${query}&access_token=09f8c4c73930306cbd5b6188b3fab565cf0e4cbf`
   })
 );
